38420ec349d263e4dadd770ae26c3da871cd5427
Shivam Rajawat <shivam.rajawat@onextel.com>
2025-09-02 16:32:28 +0530

    add: 98. Validate Binary Search Tree
    https://leetcode.com/problems/validate-binary-search-tree/description/

---
+++ src/ValidateBST.java
@@ -0,0 +1,13 @@
+class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long low, long high) {
+        if(root == null) return true;
+
+        if(root.val <= low || root.val >= high) return false;
+
+        return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
+    }
+}
